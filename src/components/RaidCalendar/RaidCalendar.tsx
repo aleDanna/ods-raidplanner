482988f27d7c0417a5b6b2e6c4dc7f5425b0dd9c
Alessio D'Anna <alessiodanna@rentalcars.com>
2020-06-10 17:07:58 +0100

    raid-calendar changes

--- src/components/RaidCalendar/RaidCalendar.tsx
+++ src/components/RaidCalendar/RaidCalendar.tsx
@@ -1,32 +1,67 @@
 import * as React from 'react';
-import {Container} from "react-bootstrap";
-import {parseISO} from "date-fns";
+import {Container, Col, Row, Media} from "react-bootstrap";
 import DayPicker from "react-day-picker";
+import {isSameDay, parseISO} from "date-fns";
 
 
 export const RaidCalendar = ({items}) => {
 
-    let result = [];
+    let events = [];
 
     items.allowedRaidGroups.map(eventGroup => {
         const title = eventGroup.groupName;
+        const imageIcon = eventGroup.imageName;
         return eventGroup.scheduledEvents.map(event => {
             return {
                 title: title,
-                start: '2020-06-17',
-                end: '2020-06-17',
-                eventClasses: 'optionalEvent',
+                start: event.start,
                 description: title,
+                icon: imageIcon
             }
         })
-    }).forEach(elem => {result = result.concat(elem)})
+    }).forEach(elem => {events = events.concat(elem)});
+
+    function renderDay(day: Date) {
+        events = events.filter(item => {
+            return isSameDay(new Date(item.start), day);
+        });
+
+        return (
+            <Container>
+                <Row>
+                    <Col md className="ods_raidplanner_raidcalendar-cell-day">
+                        {day.getDate()}
+                    </Col>
+                </Row>
+                <Container fluid className="ods_raidplanner_raidcalendar-icons">
+                    <Row className="ods_raidplanner_raidcalendar-icons">
+                        {events.map((value, _) => {
+                            return <Col md={4}>
+                                <Media>
+                                    <a className="ods_raidplanner_raidcalendar-icon-container" onClick={event => openPopup(event, value)}>
+                                        <img src={`/static/images/${value.icon}.jpg`} style={{width: "60px", height: "60px"}}/>
+                                    </a>
+                                </Media>
+                                </Col>
+                        })}
+                    </Row>
+                </Container>
+            </Container>
+        )
+    }
+
+    const openPopup = (click, event) => {
+        console.log(click);
+        console.log(event);
+    };
 
     return (
         <Container className="ods_raidplanner_raidcalendar-container">
             <DayPicker
                 canChangeMonth={false}
                 className="Raids"
+                renderDay={renderDay}
             />
         </Container>
     )
-}
+};
