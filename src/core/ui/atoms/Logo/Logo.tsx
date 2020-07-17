32ea40107c55ac29571cd54ed823943f457d9df5
aleDanna <alessio.danna.94@gmail.com>
2020-07-17 18:29:11 +0200

    logo fix

--- src/core/ui/atoms/Logo/Logo.tsx
+++ src/core/ui/atoms/Logo/Logo.tsx
@@ -2,9 +2,7 @@ import * as React from 'react';
 
 import image from '@core/assets/images/teso.png';
 
-import styles from './Logo.scss';
-
 export const Logo = () => {
 
-  return <img src={image} className={`${styles.logo} align-top`} alt="logo" />;
+  return <img src={image} className={`align-top`} alt="logo" />;
 }
