701cd2c02d757272ab8e1a49005d5246489a620e
aleDanna <alessio.danna.94@gmail.com>
2020-07-18 16:23:04 +0200

    logo fix

--- src/core/ui/atoms/Logo/Logo.tsx
+++ src/core/ui/atoms/Logo/Logo.tsx
@@ -3,8 +3,14 @@ import * as React from 'react';
 import logoMobile from '@core/assets/images/teso_mobile.png';
 import logoFull from '@core/assets/images/teso.png';
 import { isMobile } from "react-device-detect";
+import {useEffect, useState} from "react";
 
 export const Logo = () => {
+  const [image, setImage] = useState(logoFull);
 
-  return <img src={ isMobile ? logoMobile : logoFull} className={`align-top`} alt="logo" />;
+  useEffect(() => {
+    setImage(isMobile ? logoMobile : logoFull);
+  })
+  
+  return <img src={image} className={`align-top`} alt="logo" />;
 }
