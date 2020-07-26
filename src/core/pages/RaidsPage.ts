aba917dc9c26ede42f0cb55de952d383bcde1af6
aleDanna <alessio.danna.94@gmail.com>
2020-07-26 15:38:59 +0200

    fix calendar load

--- src/core/pages/RaidsPage.ts
+++ src/core/pages/RaidsPage.ts
@@ -3,48 +3,39 @@ import restClient from '../services/restClient';
 import { AsyncComponentLoader } from '@core/ui/atoms/AsyncComponentLoader/AsyncComponentLoader';
 import { ContentTitle } from '@core/ui/atoms/ContentTitle/ContentTitle';
 import pageBuilder from '@core/common/pageBuilder';
-import RaidTransformer from '@core/features/transformers/raidTransformer';
 import { RaidCalendar } from '@core/ui/components/RaidsCalendar/RaidCalendar';
 import { RaidsGrid } from '@core/ui/components/RaidsGrid/RaidsGrid';
-import { formatISODateString } from '@core/common/dateUtils';
-import sessionStorageService from '@core/services/sessionStorageService';
 
 export const RaidsPage = (routeProps) => {
   const mode = routeProps.match.params.mode;
 
-  const today = new Date();
-  const next2Weeks = new Date();
-  next2Weeks.setDate(next2Weeks.getDate() + 14);
+  async function loadRaids () {
 
-  const loadRaids = () =>
-    restClient.getRaidsByFilter({
-        startDateFilter: formatISODateString(today.toISOString(), 'yyyy-MM-dd'),
-        endDateFilter: formatISODateString(next2Weeks.toISOString(), 'yyyy-MM-dd'),
-        maxRank: sessionStorageService.get('loggedUser').rank
-    })
-      .then(data => {
-        return restClient.getSubscribedRaids()
-          .then(ids => {
-            const subscribedEvents = ids.map(row => {return row.raid_ref; });
-            const events = RaidTransformer.transformArray(data);
-            events.forEach(event => {
-              event.subscribed = subscribedEvents.indexOf(event.id) > -1;
-            });
-            return events;
-          });
-      });
+    const today = new Date();
+    const next2Weeks = new Date();
+    next2Weeks.setDate(next2Weeks.getDate() + 14);
+
+    return await restClient.getRaidsByFilter({
+      startDateFilter: today,
+      endDateFilter: next2Weeks
+    });
+  }
+
+  async function emptyLoad () {
+    console.log('loading calendar...');
+    return routeProps.history;
+  }
 
   const isCalendarView = mode === 'calendar' && !isMobile;
   const title = isCalendarView ? 'Calendario' : 'Raid disponibili';
 
   const titleComponent = ContentTitle({nameTitle: title});
-  const mainComponent = isCalendarView ?
-    RaidCalendar({history: routeProps.history}) :
+  const mainComponent =
     AsyncComponentLoader({
-      Component: RaidsGrid,
-      asyncFn: loadRaids,
-      componentProps: {events: {}, history: routeProps.history},
-      propFetched: 'events'
+      Component: isCalendarView ? RaidCalendar : RaidsGrid,
+      asyncFn: isCalendarView ? emptyLoad : loadRaids,
+      componentProps: isCalendarView ? {} : {events: {}, history: routeProps.history},
+      propFetched: isCalendarView ? 'history' : 'events'
     });
 
   return pageBuilder.build(titleComponent, mainComponent);
